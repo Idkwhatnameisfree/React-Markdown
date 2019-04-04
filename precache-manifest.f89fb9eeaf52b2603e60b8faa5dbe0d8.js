a73fd5951ef9c3b53613d50b35bfd5b498338cff
Idkwhatnameisfree <giuliano.wendt@gmail.com>
2019-04-04 14:06:17 +0200

    Updates

---
+++ precache-manifest.f89fb9eeaf52b2603e60b8faa5dbe0d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4fd9c4541629bbfe9244",
+    "url": "/static/js/main.2f63d129.chunk.js"
+  },
+  {
+    "revision": "ee80c7db0d175fd2ce9a",
+    "url": "/static/js/2.eea3572e.chunk.js"
+  },
+  {
+    "revision": "4fd9c4541629bbfe9244",
+    "url": "/static/css/main.eb9bdab4.chunk.css"
+  },
+  {
+    "revision": "5d0ccd601d19027339f4f0286e2c5c6e",
+    "url": "/index.html"
+  }
+];
